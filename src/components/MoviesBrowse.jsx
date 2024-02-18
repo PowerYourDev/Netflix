453624828a349a9fbfab6739adf4f18dbe709ca7
saiteja <saitejachicky@gmail.com>
2024-02-18 21:30:27 +0530

    fixed-issue

--- src/components/MoviesBrowse.jsx
+++ src/components/MoviesBrowse.jsx
@@ -1,26 +1,26 @@
 import React, { useEffect } from "react";
 
 import Header from "./Header";
-import { API_OPTIONS } from "../constant";
+// import { API_OPTIONS } from "../constant";
 
 const MoviesBrowse = () => {
-  const fetchNowPlayingMovies = async () => {
-     try{
-      const nowPlayingMoviesJson = await fetch(
-        "https://api.themoviedb.org/3/movie/now_playing?page=1",
-        API_OPTIONS
-      );
-      console.log("nowPlayingMoviesJson", nowPlayingMoviesJson);
-      const nowPlayingMovies = await nowPlayingMoviesJson.json();
-      console.log("nowPlayingMovies", nowPlayingMovies);
-     }
-     catch(e){
-      console.log(e)
-     }
-  };
-  useEffect(() => {
-    fetchNowPlayingMovies();
-  }, []);
+  // const fetchNowPlayingMovies = async () => {
+  //    try{
+  //     const nowPlayingMoviesJson = await fetch(
+  //       "https://api.themoviedb.org/3/movie/now_playing?page=1",
+  //       API_OPTIONS
+  //     );
+  //     console.log("nowPlayingMoviesJson", nowPlayingMoviesJson);
+  //     const nowPlayingMovies = await nowPlayingMoviesJson.json();
+  //     console.log("nowPlayingMovies", nowPlayingMovies);
+  //    }
+  //    catch(e){
+  //     console.log(e)
+  //    }
+  // };
+  // useEffect(() => {
+  //   fetchNowPlayingMovies();
+  // }, []);
 
   return (
     <div>
--- src/constant/index.js
+++ src/constant/index.js
@@ -5,10 +5,10 @@ export const netflixBgimage="https://assets.nflxext.com/ffe/siteui/vlv3/5e16108c
 export const netflixLogo="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
 
 
-export const API_OPTIONS = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OTc4NmIwNWJjMDZlZDBiNWQ0MzA5MDAzZWU0YzNjOCIsInN1YiI6IjYzODIzYzJiMWIxNTdkMDA3YTQyNmM5ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FC2TZsVuGRRdM2SQI0rDezsyvsL43aBjMUd-E2WyGmw'
-    }
-  };
+// export const API_OPTIONS = {
+//     method: 'GET',
+//     headers: {
+//       accept: 'application/json',
+//       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OTc4NmIwNWJjMDZlZDBiNWQ0MzA5MDAzZWU0YzNjOCIsInN1YiI6IjYzODIzYzJiMWIxNTdkMDA3YTQyNmM5ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FC2TZsVuGRRdM2SQI0rDezsyvsL43aBjMUd-E2WyGmw'
+//     }
+//   };
