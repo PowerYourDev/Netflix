a408d6c82d0e73956ff19d0db4a7b0d44457962d
saiteja <saitejachicky@gmail.com>
2024-02-18 21:10:21 +0530

    build-folder

--- src/components/MoviesBrowse.jsx
+++ src/components/MoviesBrowse.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import Header from './Header'
+import React, { useEffect } from "react";
+
+import Header from "./Header";
+import { API_OPTIONS } from "../constant";
 
 const MoviesBrowse = () => {
+  const fetchNowPlayingMovies = async () => {
+     try{
+      const nowPlayingMoviesJson = await fetch(
+        "https://api.themoviedb.org/3/movie/now_playing?page=1",
+        API_OPTIONS
+      );
+      console.log("nowPlayingMoviesJson", nowPlayingMoviesJson);
+      const nowPlayingMovies = await nowPlayingMoviesJson.json();
+      console.log("nowPlayingMovies", nowPlayingMovies);
+     }
+     catch(e){
+      console.log(e)
+     }
+  };
+  useEffect(() => {
+    fetchNowPlayingMovies();
+  }, []);
+
   return (
-    <div>MoviesBrowse
-
-      <Header/>
+    <div>
+      <Header />
     </div>
-  )
-}
+  );
+};
 
-export default MoviesBrowse
+export default MoviesBrowse;
--- src/constant/index.js
+++ src/constant/index.js
@@ -3,3 +3,12 @@
 
 export const netflixBgimage="https://assets.nflxext.com/ffe/siteui/vlv3/5e16108c-fd30-46de-9bb8-0b4e1bbbc509/29d8d7d7-83cc-4b5f-aa9b-6fd4f68bfaa6/IN-en-20240205-popsignuptwoweeks-perspective_alpha_website_large.jpg"
 export const netflixLogo="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OTc4NmIwNWJjMDZlZDBiNWQ0MzA5MDAzZWU0YzNjOCIsInN1YiI6IjYzODIzYzJiMWIxNTdkMDA3YTQyNmM5ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FC2TZsVuGRRdM2SQI0rDezsyvsL43aBjMUd-E2WyGmw'
+    }
+  };
